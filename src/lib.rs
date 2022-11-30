7c896cc1615495b662f4e7b00cd873b0b6995068
fleeB1 <fowler.lee8@gmail.com>
2022-11-30 22:49:53 +0100

    test for add user function

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,19 @@
 mod types;
-use std::collections::{HashMap, BTreeMap};
-use ic_cdk::export::{
-    candid::{Deserialize},
-    serde::Serialize,
-    Principal,
-};
-use ic_cdk::api::{caller as caller_api};
-use std::cell::RefCell;
+use ic_cdk::api::caller as caller_api;
+use ic_cdk::export::{candid::Deserialize, serde::Serialize, Principal};
 use ic_cdk_macros::*;
+use std::cell::RefCell;
+use std::collections::{BTreeMap, HashMap};
 
 use types::VideoStreamingCompany;
 
-thread_local!{
+thread_local! {
     static VIDEO: RefCell<VideoStreamingCompany> = RefCell::default();
     static USERS_COUNT: RefCell<u128> = RefCell::new(0u128);
     static USER_STORE: RefCell<BTreeMap<Principal, types::User>> = RefCell::new(BTreeMap::new())
 }
 
-fn get_caller() -> Principal{
+fn get_caller() -> Principal {
     let caller = caller_api();
     if caller == Principal::anonymous() {
         panic!("Anonymous principal not allowed to make calls.")
@@ -35,7 +31,7 @@ impl Default for VideoStreamingCompany {
 }
 
 impl VideoStreamingCompany {
-    fn new() -> VideoStreamingCompany{
+    fn new() -> VideoStreamingCompany {
         VideoStreamingCompany {
             user_store: HashMap::default(),
             user_count: 0u128,
@@ -49,15 +45,14 @@ impl VideoStreamingCompany {
 
 #[update(name = "add_note")]
 fn add_user(user_principal: Principal, user: types::User) {
-    USER_STORE.with(|us|
-       if let Some(_item) = us.borrow_mut().get_mut(&user_principal) {
+    USER_STORE.with(|us| {
+        if let Some(_item) = us.borrow_mut().get_mut(&user_principal) {
             // the user already exists in the storage
-       } else {
-            us.borrow_mut().insert(user_principal, user); 
-       }
-    );
+        } else {
+            us.borrow_mut().insert(user_principal, user);
+        }
+    });
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -65,6 +60,25 @@ mod tests {
     #[test]
     fn test_one() {
         let mut company = VideoStreamingCompany::new();
-        company.user_store.insert(get_caller(), types::User { name: ("lee".to_string()), wallet_id: (1i64), token_number: (10.2) });
+        company.user_store.insert(
+            get_caller(),
+            types::User {
+                name: ("lee".to_string()),
+                wallet_id: (1i64),
+                token_number: (10.2),
+            },
+        );
+    }
+    
+    #[test]
+    fn test_add_user() {
+        let x = get_caller();
+        let y = types::User {
+            name: "lee".to_string(),
+            wallet_id: 1i64,
+            token_number: 5.0,
+        };
+        USER_STORE.with(|v| v.borrow_mut().insert(x, y));
+        // let mut n = USER_STORE.with(|m|m.borrow_mut().get_key_value(&x));
     }
 }
